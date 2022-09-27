72511262e7688722bb3f88ca2bde73d215c3938b
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-09-27 23:46:19 +0500

    path

--- Python practice/PackagesPrep/message.py
+++ Python practice/PackagesPrep/message.py
@@ -1,4 +1,11 @@
+from importlib.resources import path
 from pathlib import Path
 
-path = Path("emails")
-print(path.exists())
+# path = Path("emails")
+# print(path.exists())
+
+
+path = Path()
+for file in path.glob('*.json'):
+    print(file)
+    
